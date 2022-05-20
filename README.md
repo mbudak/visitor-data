b62bdff690efd6d0a5d8ead6e7b16a7725515cf0
mbudak <promaxcoq@gmail.com>
2022-05-19 23:22:59 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # visitor-data
+
+
+https://urchin-app-bcme3.ondigitalocean.app/
--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const app = express();
 
 
 app.get("/", (req, res, next) => {
+    
+    const DeviceDetector = require('node-device-detector');
+    const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+    const detector = new DeviceDetector;
+    const result = detector.parseClient(userAgent);
+    
+
+    // console.log('ip', req.ip);
+    
     // res.sendStatus(200);
-    res.status(200).send("Hello World")
+    res.status(200).send("result");
 })
 
 var server = app.listen(port, function(){
