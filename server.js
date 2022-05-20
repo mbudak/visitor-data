3bf7b8e3dbe89c07e4b8fcf5e046b1afe7d1f2a4
mbudak <promaxcoq@gmail.com>
2022-05-20 01:49:15 -0400

    client public ip resolved

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const DeviceDetector = require('node-device-detector');
 const DeviceHelper = require('node-device-detector/helper');
 var nodemailer = require('nodemailer');
-
+var requestIp = require('request-ip');
 
 
 const express = require('express');
@@ -31,12 +31,14 @@ app.get("/", (req, res, next) => {
     const detector = new DeviceDetector;
     const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
     const result = detector.detect(userAgent);
+    
+    var clientIp = requestIp.getClientIp(req);
 
     var smptOptions = {
         from: process.env.SMTP_FROM, // sender address
         to: process.env.SMTP_TO, // list of receivers
         subject: "Data captured", // Subject line
-        text: "IP is... " + req.ip + JSON.stringify(result)
+        text: "IP is... " + clientIp + JSON.stringify(result)
         // html: "<b>Hello world ✔</b>" // html body
       };
     
